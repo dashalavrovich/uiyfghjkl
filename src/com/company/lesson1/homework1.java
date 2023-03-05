f82675771c64b2db219d05dfa3e2d91aee58fe74
dasha <dashalave17@gmail.com>
2023-03-05 21:19:09 +0300

    Выполнила первое дз

---
+++ src/com/company/lesson1/homework1.java
@@ -0,0 +1,11 @@
+package com.company.lesson1;
+
+public class homework1 {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("jngfkdr");
+    }
+}
